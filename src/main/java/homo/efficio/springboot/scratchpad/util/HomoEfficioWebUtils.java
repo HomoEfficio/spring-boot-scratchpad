be8c63cba7b6e1745cb6ea3d732dfe2d58205310
Homo Efficio <homo.efficio@gmail.com>
2017-04-15 01:37:01 +0900

    access 로그로 SpringMVC 테스트 코드 일부 생성하는 HomoEfficioWebUtils 테스트 코드 추가 및 리팩터링

--- src/main/java/homo/efficio/springboot/scratchpad/util/HomoEfficioWebUtils.java
+++ src/main/java/homo/efficio/springboot/scratchpad/util/HomoEfficioWebUtils.java
@@ -1,5 +1,6 @@
 package homo.efficio.springboot.scratchpad.util;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,14 +12,15 @@ import java.util.List;
  */
 public class HomoEfficioWebUtils {
 
+    // TODO: 배열 값이 포함된 케이스 처리 보완 필요
+
     private static final String FINAL_STRING = "final String ";
 
     public static List<String> getParamDefinitionCodesFromUrlString(String urlString) throws Exception {
 
-//        String urlString = "/v1/logs/basket/deal.11st.co.kr/14782654128048583755870?callback=jQuery112403134002401275859_1492154992123&url=http%3A%2F%2Fdeal.11st.co.kr%2Fproduct%2FSellerProductDetail.tmall%3Fmethod%3DgetSellerProductDetail%26prdNo%3D1117766194%26xfrom%3D%26xzone%3D&ref=http%3A%2F%2Fbuy.11st.co.kr%2Forder%2FOrderList.tmall&b=eyJ1c2VyIjp7ImJpcnRoeWVhciI6IjE5NTgiLCJnZW5kZXIiOiJGIiwibWlkIjoiNTQ2NjMzNzg3Njg5MjE5MTIxNyJ9fQ%3D%3D&items%5B0%5D%5Bid%5D=1117766194&items%5B0%5D%5Bc1%5D=+%EC%9E%90%EB%8F%99%EC%B0%A8%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc2%5D=+%EC%84%B8%EC%B0%A8%2F%EA%B4%80%EB%A6%AC%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc3%5D=+%EC%B9%B4%EC%83%B4%ED%91%B8&items%5B0%5D%5Bc4%5D=0&items%5B0%5D%5Bcount%5D=3&items%5B0%5D%5Boptions%5D%5B0%5D%5Bstck_no%5D=6135264233&items%5B0%5D%5Boptions%5D%5B0%5D%5Bopt_no%5D=1%3A2&items%5B0%5D%5Boptions%5D%5B0%5D%5Bname%5D=A11.%EC%86%8C%EB%82%99%EC%8A%A4+%EB%A9%80%ED%8B%B0%EB%B0%94%EC%8A%A4%EC%BC%93+-+8L&items%5B0%5D%5Boptions%5D%5B0%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B1%5D%5Bstck_no%5D=6031636659&items%5B0%5D%5Boptions%5D%5B1%5D%5Bopt_no%5D=1%3A13&items%5B0%5D%5Boptions%5D%5B1%5D%5Bname%5D=C03.%EC%86%8C%EB%82%99%EC%8A%A4+%EA%B3%A0%EC%86%8D%EC%BD%94%ED%8C%85%EC%99%81%EC%8A%A4+-+500ml&items%5B0%5D%5Boptions%5D%5B1%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B2%5D%5Bstck_no%5D=6031636679&items%5B0%5D%5Boptions%5D%5B2%5D%5Bopt_no%5D=1%3A32&items%5B0%5D%5Boptions%5D%5B2%5D%5Bname%5D=F01.%EC%86%8C%EB%82%99%EC%8A%A4+%EC%BB%B4%ED%8C%8C%EC%9A%B4%EB%93%9C+-+75ml&items%5B0%5D%5Boptions%5D%5B2%5D%5Bqty%5D=1&_=1492154992124";
+//        String urlString = "/v1/logs/basket/deal.11st.co.kr/14782654128048583755870?callback=jQuery112403134002401275859_1492154992123&url=http%3A%2F%2Fdeal.11st.co.kr%2Fproduct%2FSellerProductDetail.tmall%3Fmethod%3DgetSellerProductDetail%26prdNo%3D1117766194%26xfrom%3D%26xzone%3D&ref=http%3A%2F%2Fbuy.11st.co.kr%2Forder%2FOrderList.tmall&b=eyJ1c2VyIjp7ImJpcnRoeWVhciI6IjE5NTgiLCJnZW5kZXIiOiJGIiwibWlkIjoiNTQ2NjMzNzg3Njg5MjE5MTIxNyJ9fQ%3D%3D&items%5B0%5D%5Bid%5D=1117766194&items%5B0%5D%5Bc1%5D=+%EC%9E%90%EB%8F%99%EC%B0%A8%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc2%5D=+%EC%84%B8%EC%B0%A8%2F%EA%B4%80%EB%A6%AC%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc3%5D=+%EC%B9%B4%EC%83%B4%ED%91%B8&items%5B0%5D%5Bc4%5D=0&items%5B0%5D%5Bcount%5D=3&items%5B0%5D%5Bcatalog%5D=%5B0%2C%20%EC%B9%B4%ED%83%88%EB%A1%9C%EA%B7%B81%2C%20%EC%B9%B4%ED%83%88%EB%A1%9C%EA%B7%B82%2C%203%5D&items%5B0%5D%5Boptions%5D%5B0%5D%5Bstck_no%5D=6135264233&items%5B0%5D%5Boptions%5D%5B0%5D%5Bopt_no%5D=1%3A2&items%5B0%5D%5Boptions%5D%5B0%5D%5Bname%5D=A11.%EC%86%8C%EB%82%99%EC%8A%A4+%EB%A9%80%ED%8B%B0%EB%B0%94%EC%8A%A4%EC%BC%93+-+8L&items%5B0%5D%5Boptions%5D%5B0%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B1%5D%5Bstck_no%5D=6031636659&items%5B0%5D%5Boptions%5D%5B1%5D%5Bopt_no%5D=1%3A13&items%5B0%5D%5Boptions%5D%5B1%5D%5Bname%5D=C03.%EC%86%8C%EB%82%99%EC%8A%A4+%EA%B3%A0%EC%86%8D%EC%BD%94%ED%8C%85%EC%99%81%EC%8A%A4+-+500ml&items%5B0%5D%5Boptions%5D%5B1%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B2%5D%5Bstck_no%5D=6031636679&items%5B0%5D%5Boptions%5D%5B2%5D%5Bopt_no%5D=1%3A32&items%5B0%5D%5Boptions%5D%5B2%5D%5Bname%5D=F01.%EC%86%8C%EB%82%99%EC%8A%A4+%EC%BB%B4%ED%8C%8C%EC%9A%B4%EB%93%9C+-+75ml&items%5B0%5D%5Boptions%5D%5B2%5D%5Bqty%5D=1&_=1492154992124";
 
-        String urlDecodedString = URLDecoder.decode(urlString, "UTF-8");
-        String urlParamsString = urlDecodedString.substring(urlDecodedString.indexOf('?') + 1);
+        String urlParamsString = getURLDecodedParamsString(urlString);
 
         final List<String[]> defParamsByLines = getFirstKVStrings(urlParamsString);
 
@@ -37,19 +39,18 @@ public class HomoEfficioWebUtils {
     }
 
     /**
-     * 
+     *
      * @param s
      * @return List<String[] {"final String key", "value"}>
      */
     private static List<String[]> getFirstKVStrings(String s) {
-        final String urlParamsString = "&" + s;
-        final String splittedByAmpersand = urlParamsString.replaceAll("\\&", "\n" + FINAL_STRING);
-        final List<String> urlParamsByLines = Arrays.asList(splittedByAmpersand.substring(1).split("\n"));
+        final String splittedByAmpersand = s.replaceAll("\\&", "\n");
+        final List<String> urlParamsByLines = Arrays.asList(splittedByAmpersand.split("\n"));
         final List<String[]> defParamsByLines = new ArrayList<>();
 
         for (String line : urlParamsByLines) {
             final String[] paramTuple = new String[] {"", ""};
-            paramTuple[0] = line.substring(0, line.indexOf("="));
+            paramTuple[0] = FINAL_STRING + line.substring(0, line.indexOf("="));
             paramTuple[1] = line.substring(line.indexOf("=") + 1);
 
             defParamsByLines.add(paramTuple);
@@ -57,21 +58,27 @@ public class HomoEfficioWebUtils {
         return defParamsByLines;
     }
 
-    public void getParamMethodCodes() throws Exception {
+    public static List<String> getParamMethodCodes(String urlString) throws Exception {
 
-        String urlString = "callback=jQuery112403134002401275859_1492154992123&url=http://deal.11st.co.kr/product/SellerProductDetail.tmall?method=getSellerProductDetail&prdNo=1117766194&xfrom=&xzone=&ref=http://buy.11st.co.kr/order/OrderList.tmall&b=eyJ1c2VyIjp7ImJpcnRoeWVhciI6IjE5NTgiLCJnZW5kZXIiOiJGIiwibWlkIjoiNTQ2NjMzNzg3Njg5MjE5MTIxNyJ9fQ==&items[0][id]=1117766194&items[0][c1]= 자동차용품 &items[0][c2]= 세차/관리용품 &items[0][c3]= 카샴푸&items[0][c4]=0&items[0][count]=3&items[0][options][0][stck_no]=6135264233&items[0][options][0][opt_no]=1:2&items[0][options][0][name]=A11.소낙스 멀티바스켓 - 8L&items[0][options][0][qty]=1&items[0][options][1][stck_no]=6031636659&items[0][options][1][opt_no]=1:13&items[0][options][1][name]=C03.소낙스 고속코팅왁스 - 500ml&items[0][options][1][qty]=1&items[0][options][2][stck_no]=6031636679&items[0][options][2][opt_no]=1:32&items[0][options][2][name]=F01.소낙스 컴파운드 - 75ml&items[0][options][2][qty]=1&_=1492154992124";
+//        String urlString = "/v1/logs/basket/deal.11st.co.kr/14782654128048583755870?callback=jQuery112403134002401275859_1492154992123&url=http%3A%2F%2Fdeal.11st.co.kr%2Fproduct%2FSellerProductDetail.tmall%3Fmethod%3DgetSellerProductDetail%26prdNo%3D1117766194%26xfrom%3D%26xzone%3D&ref=http%3A%2F%2Fbuy.11st.co.kr%2Forder%2FOrderList.tmall&b=eyJ1c2VyIjp7ImJpcnRoeWVhciI6IjE5NTgiLCJnZW5kZXIiOiJGIiwibWlkIjoiNTQ2NjMzNzg3Njg5MjE5MTIxNyJ9fQ%3D%3D&items%5B0%5D%5Bid%5D=1117766194&items%5B0%5D%5Bc1%5D=+%EC%9E%90%EB%8F%99%EC%B0%A8%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc2%5D=+%EC%84%B8%EC%B0%A8%2F%EA%B4%80%EB%A6%AC%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc3%5D=+%EC%B9%B4%EC%83%B4%ED%91%B8&items%5B0%5D%5Bc4%5D=0&items%5B0%5D%5Bcount%5D=3&items%5B0%5D%5Bcatalog%5D=%5B0%2C%20%EC%B9%B4%ED%83%88%EB%A1%9C%EA%B7%B81%2C%20%EC%B9%B4%ED%83%88%EB%A1%9C%EA%B7%B82%2C%203%5D&items%5B0%5D%5Boptions%5D%5B0%5D%5Bstck_no%5D=6135264233&items%5B0%5D%5Boptions%5D%5B0%5D%5Bopt_no%5D=1%3A2&items%5B0%5D%5Boptions%5D%5B0%5D%5Bname%5D=A11.%EC%86%8C%EB%82%99%EC%8A%A4+%EB%A9%80%ED%8B%B0%EB%B0%94%EC%8A%A4%EC%BC%93+-+8L&items%5B0%5D%5Boptions%5D%5B0%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B1%5D%5Bstck_no%5D=6031636659&items%5B0%5D%5Boptions%5D%5B1%5D%5Bopt_no%5D=1%3A13&items%5B0%5D%5Boptions%5D%5B1%5D%5Bname%5D=C03.%EC%86%8C%EB%82%99%EC%8A%A4+%EA%B3%A0%EC%86%8D%EC%BD%94%ED%8C%85%EC%99%81%EC%8A%A4+-+500ml&items%5B0%5D%5Boptions%5D%5B1%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B2%5D%5Bstck_no%5D=6031636679&items%5B0%5D%5Boptions%5D%5B2%5D%5Bopt_no%5D=1%3A32&items%5B0%5D%5Boptions%5D%5B2%5D%5Bname%5D=F01.%EC%86%8C%EB%82%99%EC%8A%A4+%EC%BB%B4%ED%8C%8C%EC%9A%B4%EB%93%9C+-+75ml&items%5B0%5D%5Boptions%5D%5B2%5D%5Bqty%5D=1&_=1492154992124";
 
-        final List<String[]> firstKVStrings = getFirstKVStrings(urlString.split("\\?")[1]);
+        String urlParamsString = getURLDecodedParamsString(urlString);
+
+        final List<String[]> firstKVStrings = getFirstKVStrings(urlParamsString);
 
         final List<String> paramMethodsLines = new ArrayList<>();
         for (String[] tuple : firstKVStrings) {
             paramMethodsLines.add(
-                    ".param(\"" + tuple[0].substring(FINAL_STRING.length()) + "\", \"" + convertSquareBracketToUnderscore(tuple[1]) + "\")"
+                    ".param(\"" + tuple[0].substring(FINAL_STRING.length()) + "\", " + convertSquareBracketToUnderscore(tuple[0].substring(FINAL_STRING.length())) + ")"
             );
         }
 
-        System.out.println(paramMethodsLines);
-//        return paramMethodsLines;
+        return paramMethodsLines;
+    }
+
+    private static String getURLDecodedParamsString(String urlString) throws UnsupportedEncodingException {
+        String urlDecodedString = URLDecoder.decode(urlString, "UTF-8");
+        return urlDecodedString.substring(urlDecodedString.indexOf('?') + 1);
     }
 
     private static String convertSquareBracketToUnderscore(String s) {
---
+++ src/test/java/homo/efficio/springboot/scratchpad/util/HomoEfficioWebUtilsTest.java
@@ -0,0 +1,117 @@
+package homo.efficio.springboot.scratchpad.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author homo.efficio@gmail.com
+ *         created on 2017-04-14
+ */
+@RunWith(JUnit4.class)
+public class HomoEfficioWebUtilsTest {
+
+    private static final String FINAL_STRING = "final String ";
+
+    @Test
+    public void URL_String에서_ParamKey_ParamValue_정의문_생성_테스트() throws Exception {
+
+        // TODO: 배열 값이 포함된 케이스 처리 보완 필요
+
+        // 배열 값 없음
+        String urlString = "/v1/logs/basket/deal.11st.co.kr/14782654128048583755870?callback=jQuery112403134002401275859_1492154992123&url=http%3A%2F%2Fdeal.11st.co.kr%2Fproduct%2FSellerProductDetail.tmall%3Fmethod%3DgetSellerProductDetail%26prdNo%3D1117766194%26xfrom%3D%26xzone%3D&ref=http%3A%2F%2Fbuy.11st.co.kr%2Forder%2FOrderList.tmall&b=eyJ1c2VyIjp7ImJpcnRoeWVhciI6IjE5NTgiLCJnZW5kZXIiOiJGIiwibWlkIjoiNTQ2NjMzNzg3Njg5MjE5MTIxNyJ9fQ%3D%3D&items%5B0%5D%5Bid%5D=1117766194&items%5B0%5D%5Bc1%5D=+%EC%9E%90%EB%8F%99%EC%B0%A8%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc2%5D=+%EC%84%B8%EC%B0%A8%2F%EA%B4%80%EB%A6%AC%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc3%5D=+%EC%B9%B4%EC%83%B4%ED%91%B8&items%5B0%5D%5Bc4%5D=0&items%5B0%5D%5Bcount%5D=3&items%5B0%5D%5Boptions%5D%5B0%5D%5Bstck_no%5D=6135264233&items%5B0%5D%5Boptions%5D%5B0%5D%5Bopt_no%5D=1%3A2&items%5B0%5D%5Boptions%5D%5B0%5D%5Bname%5D=A11.%EC%86%8C%EB%82%99%EC%8A%A4+%EB%A9%80%ED%8B%B0%EB%B0%94%EC%8A%A4%EC%BC%93+-+8L&items%5B0%5D%5Boptions%5D%5B0%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B1%5D%5Bstck_no%5D=6031636659&items%5B0%5D%5Boptions%5D%5B1%5D%5Bopt_no%5D=1%3A13&items%5B0%5D%5Boptions%5D%5B1%5D%5Bname%5D=C03.%EC%86%8C%EB%82%99%EC%8A%A4+%EA%B3%A0%EC%86%8D%EC%BD%94%ED%8C%85%EC%99%81%EC%8A%A4+-+500ml&items%5B0%5D%5Boptions%5D%5B1%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B2%5D%5Bstck_no%5D=6031636679&items%5B0%5D%5Boptions%5D%5B2%5D%5Bopt_no%5D=1%3A32&items%5B0%5D%5Boptions%5D%5B2%5D%5Bname%5D=F01.%EC%86%8C%EB%82%99%EC%8A%A4+%EC%BB%B4%ED%8C%8C%EC%9A%B4%EB%93%9C+-+75ml&items%5B0%5D%5Boptions%5D%5B2%5D%5Bqty%5D=1&_=1492154992124";
+
+        // 배열 값 있음
+//        String urlString = "/v1/logs/basket/deal.11st.co.kr/14782654128048583755870?callback=jQuery112403134002401275859_1492154992123&url=http%3A%2F%2Fdeal.11st.co.kr%2Fproduct%2FSellerProductDetail.tmall%3Fmethod%3DgetSellerProductDetail%26prdNo%3D1117766194%26xfrom%3D%26xzone%3D&ref=http%3A%2F%2Fbuy.11st.co.kr%2Forder%2FOrderList.tmall&b=eyJ1c2VyIjp7ImJpcnRoeWVhciI6IjE5NTgiLCJnZW5kZXIiOiJGIiwibWlkIjoiNTQ2NjMzNzg3Njg5MjE5MTIxNyJ9fQ%3D%3D&items%5B0%5D%5Bid%5D=1117766194&items%5B0%5D%5Bc1%5D=+%EC%9E%90%EB%8F%99%EC%B0%A8%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc2%5D=+%EC%84%B8%EC%B0%A8%2F%EA%B4%80%EB%A6%AC%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc3%5D=+%EC%B9%B4%EC%83%B4%ED%91%B8&items%5B0%5D%5Bc4%5D=0&items%5B0%5D%5Bcount%5D=3&items%5B0%5D%5Bcatalog%5D=%5B0%2C%20%EC%B9%B4%ED%83%88%EB%A1%9C%EA%B7%B81%2C%20%EC%B9%B4%ED%83%88%EB%A1%9C%EA%B7%B82%2C%203%5D&items%5B0%5D%5Boptions%5D%5B0%5D%5Bstck_no%5D=6135264233&items%5B0%5D%5Boptions%5D%5B0%5D%5Bopt_no%5D=1%3A2&items%5B0%5D%5Boptions%5D%5B0%5D%5Bname%5D=A11.%EC%86%8C%EB%82%99%EC%8A%A4+%EB%A9%80%ED%8B%B0%EB%B0%94%EC%8A%A4%EC%BC%93+-+8L&items%5B0%5D%5Boptions%5D%5B0%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B1%5D%5Bstck_no%5D=6031636659&items%5B0%5D%5Boptions%5D%5B1%5D%5Bopt_no%5D=1%3A13&items%5B0%5D%5Boptions%5D%5B1%5D%5Bname%5D=C03.%EC%86%8C%EB%82%99%EC%8A%A4+%EA%B3%A0%EC%86%8D%EC%BD%94%ED%8C%85%EC%99%81%EC%8A%A4+-+500ml&items%5B0%5D%5Boptions%5D%5B1%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B2%5D%5Bstck_no%5D=6031636679&items%5B0%5D%5Boptions%5D%5B2%5D%5Bopt_no%5D=1%3A32&items%5B0%5D%5Boptions%5D%5B2%5D%5Bname%5D=F01.%EC%86%8C%EB%82%99%EC%8A%A4+%EC%BB%B4%ED%8C%8C%EC%9A%B4%EB%93%9C+-+75ml&items%5B0%5D%5Boptions%5D%5B2%5D%5Bqty%5D=1&_=1492154992124";
+
+        List<String> actualResults = HomoEfficioWebUtils.getParamDefinitionCodesFromUrlString(urlString);
+
+        List<String> expectedResults = new ArrayList<>();
+        expectedResults.add("final String callback = \"jQuery112403134002401275859_1492154992123\";");
+        expectedResults.add("final String url = \"http://deal.11st.co.kr/product/SellerProductDetail.tmall?method=getSellerProductDetail\";");
+        expectedResults.add("final String prdNo = \"1117766194\";");
+        expectedResults.add("final String xfrom = \"\";");
+        expectedResults.add("final String xzone = \"\";");
+        expectedResults.add("final String ref = \"http://buy.11st.co.kr/order/OrderList.tmall\";");
+        expectedResults.add("final String b = \"eyJ1c2VyIjp7ImJpcnRoeWVhciI6IjE5NTgiLCJnZW5kZXIiOiJGIiwibWlkIjoiNTQ2NjMzNzg3Njg5MjE5MTIxNyJ9fQ==\";");
+        expectedResults.add("final String items_0_id = \"1117766194\";");
+        expectedResults.add("final String items_0_c1 = \" 자동차용품 \";");
+        expectedResults.add("final String items_0_c2 = \" 세차/관리용품 \";");
+        expectedResults.add("final String items_0_c3 = \" 카샴푸\";");
+        expectedResults.add("final String items_0_c4 = \"0\";");
+        expectedResults.add("final String items_0_count = \"3\";");
+//        expectedResults.add("final String items_0_catalog = [0, \"카탈로그1\", \"카탈로그2\", 3];");
+        expectedResults.add("final String items_0_options_0_stck_no = \"6135264233\";");
+        expectedResults.add("final String items_0_options_0_opt_no = \"1:2\";");
+        expectedResults.add("final String items_0_options_0_name = \"A11.소낙스 멀티바스켓 - 8L\";");
+        expectedResults.add("final String items_0_options_0_qty = \"1\";");
+        expectedResults.add("final String items_0_options_1_stck_no = \"6031636659\";");
+        expectedResults.add("final String items_0_options_1_opt_no = \"1:13\";");
+        expectedResults.add("final String items_0_options_1_name = \"C03.소낙스 고속코팅왁스 - 500ml\";");
+        expectedResults.add("final String items_0_options_1_qty = \"1\";");
+        expectedResults.add("final String items_0_options_2_stck_no = \"6031636679\";");
+        expectedResults.add("final String items_0_options_2_opt_no = \"1:32\";");
+        expectedResults.add("final String items_0_options_2_name = \"F01.소낙스 컴파운드 - 75ml\";");
+        expectedResults.add("final String items_0_options_2_qty = \"1\";");
+        expectedResults.add("final String _ = \"1492154992124\";");
+
+        assertThat("url parameter의 개수가 다름", actualResults.size(), is(expectedResults.size()));
+
+        for (int i = 0, len = actualResults.size(); i < len ; i++) {
+            assertThat(actualResults.get(i), is(expectedResults.get(i)));
+        }
+    }
+
+    @Test
+    public void URL_String에서_SpringMVC_param메서드구문_생성_테스트() throws Exception {
+
+        // 배열 값 없음
+        String urlString = "/v1/logs/basket/deal.11st.co.kr/14782654128048583755870?callback=jQuery112403134002401275859_1492154992123&url=http%3A%2F%2Fdeal.11st.co.kr%2Fproduct%2FSellerProductDetail.tmall%3Fmethod%3DgetSellerProductDetail%26prdNo%3D1117766194%26xfrom%3D%26xzone%3D&ref=http%3A%2F%2Fbuy.11st.co.kr%2Forder%2FOrderList.tmall&b=eyJ1c2VyIjp7ImJpcnRoeWVhciI6IjE5NTgiLCJnZW5kZXIiOiJGIiwibWlkIjoiNTQ2NjMzNzg3Njg5MjE5MTIxNyJ9fQ%3D%3D&items%5B0%5D%5Bid%5D=1117766194&items%5B0%5D%5Bc1%5D=+%EC%9E%90%EB%8F%99%EC%B0%A8%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc2%5D=+%EC%84%B8%EC%B0%A8%2F%EA%B4%80%EB%A6%AC%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc3%5D=+%EC%B9%B4%EC%83%B4%ED%91%B8&items%5B0%5D%5Bc4%5D=0&items%5B0%5D%5Bcount%5D=3&items%5B0%5D%5Boptions%5D%5B0%5D%5Bstck_no%5D=6135264233&items%5B0%5D%5Boptions%5D%5B0%5D%5Bopt_no%5D=1%3A2&items%5B0%5D%5Boptions%5D%5B0%5D%5Bname%5D=A11.%EC%86%8C%EB%82%99%EC%8A%A4+%EB%A9%80%ED%8B%B0%EB%B0%94%EC%8A%A4%EC%BC%93+-+8L&items%5B0%5D%5Boptions%5D%5B0%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B1%5D%5Bstck_no%5D=6031636659&items%5B0%5D%5Boptions%5D%5B1%5D%5Bopt_no%5D=1%3A13&items%5B0%5D%5Boptions%5D%5B1%5D%5Bname%5D=C03.%EC%86%8C%EB%82%99%EC%8A%A4+%EA%B3%A0%EC%86%8D%EC%BD%94%ED%8C%85%EC%99%81%EC%8A%A4+-+500ml&items%5B0%5D%5Boptions%5D%5B1%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B2%5D%5Bstck_no%5D=6031636679&items%5B0%5D%5Boptions%5D%5B2%5D%5Bopt_no%5D=1%3A32&items%5B0%5D%5Boptions%5D%5B2%5D%5Bname%5D=F01.%EC%86%8C%EB%82%99%EC%8A%A4+%EC%BB%B4%ED%8C%8C%EC%9A%B4%EB%93%9C+-+75ml&items%5B0%5D%5Boptions%5D%5B2%5D%5Bqty%5D=1&_=1492154992124";
+
+        // 배열 값 있음
+//        String urlString = "/v1/logs/basket/deal.11st.co.kr/14782654128048583755870?callback=jQuery112403134002401275859_1492154992123&url=http%3A%2F%2Fdeal.11st.co.kr%2Fproduct%2FSellerProductDetail.tmall%3Fmethod%3DgetSellerProductDetail%26prdNo%3D1117766194%26xfrom%3D%26xzone%3D&ref=http%3A%2F%2Fbuy.11st.co.kr%2Forder%2FOrderList.tmall&b=eyJ1c2VyIjp7ImJpcnRoeWVhciI6IjE5NTgiLCJnZW5kZXIiOiJGIiwibWlkIjoiNTQ2NjMzNzg3Njg5MjE5MTIxNyJ9fQ%3D%3D&items%5B0%5D%5Bid%5D=1117766194&items%5B0%5D%5Bc1%5D=+%EC%9E%90%EB%8F%99%EC%B0%A8%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc2%5D=+%EC%84%B8%EC%B0%A8%2F%EA%B4%80%EB%A6%AC%EC%9A%A9%ED%92%88+&items%5B0%5D%5Bc3%5D=+%EC%B9%B4%EC%83%B4%ED%91%B8&items%5B0%5D%5Bc4%5D=0&items%5B0%5D%5Bcount%5D=3&items%5B0%5D%5Bcatalog%5D=%5B0%2C%20%EC%B9%B4%ED%83%88%EB%A1%9C%EA%B7%B81%2C%20%EC%B9%B4%ED%83%88%EB%A1%9C%EA%B7%B82%2C%203%5D&items%5B0%5D%5Boptions%5D%5B0%5D%5Bstck_no%5D=6135264233&items%5B0%5D%5Boptions%5D%5B0%5D%5Bopt_no%5D=1%3A2&items%5B0%5D%5Boptions%5D%5B0%5D%5Bname%5D=A11.%EC%86%8C%EB%82%99%EC%8A%A4+%EB%A9%80%ED%8B%B0%EB%B0%94%EC%8A%A4%EC%BC%93+-+8L&items%5B0%5D%5Boptions%5D%5B0%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B1%5D%5Bstck_no%5D=6031636659&items%5B0%5D%5Boptions%5D%5B1%5D%5Bopt_no%5D=1%3A13&items%5B0%5D%5Boptions%5D%5B1%5D%5Bname%5D=C03.%EC%86%8C%EB%82%99%EC%8A%A4+%EA%B3%A0%EC%86%8D%EC%BD%94%ED%8C%85%EC%99%81%EC%8A%A4+-+500ml&items%5B0%5D%5Boptions%5D%5B1%5D%5Bqty%5D=1&items%5B0%5D%5Boptions%5D%5B2%5D%5Bstck_no%5D=6031636679&items%5B0%5D%5Boptions%5D%5B2%5D%5Bopt_no%5D=1%3A32&items%5B0%5D%5Boptions%5D%5B2%5D%5Bname%5D=F01.%EC%86%8C%EB%82%99%EC%8A%A4+%EC%BB%B4%ED%8C%8C%EC%9A%B4%EB%93%9C+-+75ml&items%5B0%5D%5Boptions%5D%5B2%5D%5Bqty%5D=1&_=1492154992124";
+
+        List<String> actualResults = HomoEfficioWebUtils.getParamMethodCodes(urlString);
+
+        List<String> expectedResults = new ArrayList<>();
+        expectedResults.add(".param(\"callback\", callback)");
+        expectedResults.add(".param(\"url\", url)");
+        expectedResults.add(".param(\"prdNo\", prdNo)");
+        expectedResults.add(".param(\"xfrom\", xfrom)");
+        expectedResults.add(".param(\"xzone\", xzone)");
+        expectedResults.add(".param(\"ref\", ref)");
+        expectedResults.add(".param(\"b\", b)");
+        expectedResults.add(".param(\"items[0][id]\", items_0_id)");
+        expectedResults.add(".param(\"items[0][c1]\", items_0_c1)");
+        expectedResults.add(".param(\"items[0][c2]\", items_0_c2)");
+        expectedResults.add(".param(\"items[0][c3]\", items_0_c3)");
+        expectedResults.add(".param(\"items[0][c4]\", items_0_c4)");
+        expectedResults.add(".param(\"items[0][count]\", items_0_count)");
+//        expectedResults.add(".param(\"items[0][catalog]\", items_0_catalog)");
+        expectedResults.add(".param(\"items[0][options][0][stck_no]\", items_0_options_0_stck_no)");
+        expectedResults.add(".param(\"items[0][options][0][opt_no]\", items_0_options_0_opt_no)");
+        expectedResults.add(".param(\"items[0][options][0][name]\", items_0_options_0_name)");
+        expectedResults.add(".param(\"items[0][options][0][qty]\", items_0_options_0_qty)");
+        expectedResults.add(".param(\"items[0][options][1][stck_no]\", items_0_options_1_stck_no)");
+        expectedResults.add(".param(\"items[0][options][1][opt_no]\", items_0_options_1_opt_no)");
+        expectedResults.add(".param(\"items[0][options][1][name]\", items_0_options_1_name)");
+        expectedResults.add(".param(\"items[0][options][1][qty]\", items_0_options_1_qty)");
+        expectedResults.add(".param(\"items[0][options][2][stck_no]\", items_0_options_2_stck_no)");
+        expectedResults.add(".param(\"items[0][options][2][opt_no]\", items_0_options_2_opt_no)");
+        expectedResults.add(".param(\"items[0][options][2][name]\", items_0_options_2_name)");
+        expectedResults.add(".param(\"items[0][options][2][qty]\", items_0_options_2_qty)");
+        expectedResults.add(".param(\"_\", _)");
+
+        assertThat("url parameter의 개수가 다름", actualResults.size(), is(expectedResults.size()));
+
+        for (int i = 0, len = actualResults.size(); i < len ; i++) {
+            assertThat(actualResults.get(i), is(expectedResults.get(i)));
+        }
+    }
+}
